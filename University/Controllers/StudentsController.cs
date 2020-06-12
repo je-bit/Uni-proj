4a55ce0c1ce5f5aba6724c896e71b8071d1f0641
je-bit <djenni_2001@abv.bg>
2020-06-12 14:21:02 +0300

    Add background

---
+++ University/Controllers/StudentsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace University.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly University.Data.ApplicationDbContext _context;
+
+        public StudentsController(University.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //[HttpGet]
+        //public async Task<IActionResult> Index()
+        //{
+
+        //    return View(courses);
+        //}
+
+    }
+}
